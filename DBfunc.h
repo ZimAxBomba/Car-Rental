f7153a26c382e7b086547dbb30e1a70304e320f6
ZimAxPlomba2137 <zima@debian-BULLSEYE-live-builder-AMD64>
2023-03-10 13:16:28 +0100

    struct v2

--- DBfunc.h
+++ DBfunc.h
@@ -1,20 +1,33 @@
 #include <string.h>
-struct Car{
+struct Auto{
+	int nr_samochodu;
 	int rejestracja;
 	char[20] marka;
 	char[50] model;
 	int rok;
 	char[10] kolor;
 	int przebieg;
-	int ilosc;
-	int ilosc_wyp;
 
 };
 
 struct Osoba{
+	int nr_klienta;
 	int karta;
 	char[20] imie;
 	char[50] nazwisko;
 	char[50] adres;
 	int telefon;
 };
+
+struct Wypozyczenia{
+	int nr_wyp;
+	int nr_klienta;
+	int nr_samochodu;
+	int data_wyp;
+	int data_zwrotu;
+	float kaucja;
+	float cena;
+
+};
+
+
